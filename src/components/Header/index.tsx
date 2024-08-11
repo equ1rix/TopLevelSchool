c9442920f77680e2838ddb57ef9c5dd43a02f28e
equ1rix <103569669+equ1rix@users.noreply.github.com>
2024-08-11 18:05:18 +0200

    School-2: Add Mainpage component
    * task(src): add header to mainpage
    
    * task(Firstpart): add firstpart on mainpage

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+const Header = () => {
+  const navArray = [
+    { id: "1", title: "Home", link: "#home" },
+    { id: "2", title: "Info", link: "#info" },
+    { id: "3", title: "Contacts", link: "#contacts" },
+  ];
+
+  return (
+    <header className="h-[116px] bg-gray-800 text-white flex items-center">
+      <div className="container mx-auto max-w-[937px] flex items-center">
+        <div className="text-xl font-bold">
+          <a href="#home">Logo</a>
+        </div>
+        <div className=" container flex justify-end items-center">
+          <nav>
+            <ul className="flex space-x-20">
+              {navArray.map((el) => (
+                <li key={el.id}>
+                  <a
+                    href={el.link}
+                    className="relative text-white group font-semibold"
+                  >
+                    {el.title}
+                    <span className="absolute block h-[2px] bg-gray-400 left-0 bottom-0 w-0 group-hover:w-full transition-all duration-300"></span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="ml-[100px]">Account</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Mainpage/index.tsx
@@ -0,0 +1,13 @@
+import Firstpart from "../../components/Firstpart";
+import Header from "../../components/Header";
+
+const Mainpage = () => {
+  return (
+    <>
+      <Header />
+      <Firstpart />
+    </>
+  );
+};
+
+export default Mainpage;
